87422d14ca11dcad118b76247e9da91492c7650f
min <mjnorthwesternwork1121@gmail.com>
2019-01-29 20:12:30 -0600

    More code and files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Southwestern Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chipotle Black Bean Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Cheddar Burger');
