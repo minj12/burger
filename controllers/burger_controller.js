81dada587768b074b417cf058803e8ceabbb918f
min <mjnorthwesternwork1121@gmail.com>
2019-01-31 15:15:21 -0600

    added syntax and updated SQL

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -14,3 +14,43 @@ router.get("/", (req, res) => {
         res.render("index", hbsObject); 
     })
 });
+
+router.get("/", (req, res) => {
+    burger.all(data => {
+        var hbsObject = {
+            burgers: data 
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject); 
+    })
+});
+
+// Create Burger 
+router.post("/api/burgers", (req, res) => {
+    burger.insert([
+        "burger_name", "devoured"
+    ], [req.body.burger_name, req.body.devoured], result => {
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+// Update Burger from list to devoured
+router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, result => {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Export routes for server.js
+module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,15 @@
-INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Turkey Burger');
-INSERT INTO burgers (burger_name) VALUES ('Southwestern Veggie Burger');
-INSERT INTO burgers (burger_name) VALUES ('Chipotle Black Bean Burger');
-INSERT INTO burgers (burger_name) VALUES ('Jalapeno Cheddar Burger');
+/*Inserting values in 'burgers' to MySQL table*/
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Mushroom Swiss Turkey Burger');
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Southwestern Veggie Burger');
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Chipotle Black Bean Burger');
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Jalapeno Cheddar Burger');
